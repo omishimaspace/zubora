202512c5d3d476f3f8661e56754589efac64ea41
nof <sakape@gmail.com>
2018-12-01 14:17:23 +0900

    add step

---
+++ app/models/step.rb
@@ -0,0 +1,3 @@
+class Step < ApplicationRecord
+  belongs_to :recipes
+end
---
+++ db/migrate/20181201051626_create_steps.rb
@@ -0,0 +1,11 @@
+class CreateSteps < ActiveRecord::Migration[5.2]
+  def change
+    create_table :steps do |t|
+      t.references :recipes, foreign_key: true
+      t.integer :position
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
